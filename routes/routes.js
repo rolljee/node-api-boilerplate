a3dccce8a576cc8221b9f099924a8e5460bcb0d7
rolljee <n.riquelmebareiro@gmail.com>
2019-01-03 15:45:36 +0100

    Working on submit method

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
-const SpeechMatics = require('../src/speechmatics');
+const SpeechMatics = require("../src/speechmatics");
 
 const appRouter = function(app) {
   app.post("/submit", function(req, res) {
-    res.status(200).send("You just started a new Job");
+    SpeechMatics.submit(req, res);
+    res.status(200).send("Job started sucessfully");
   });
   app.get("/status/:pid", function(req, res) {
+    SpeechMatics.monitor(req, res);
     res.status(200).send("You should be monitoring the speechmatics");
   });
 };
--- src/speechmatics.js
+++ src/speechmatics.js
@@ -1,10 +1,27 @@
+const cp = require("child_process");
+
 const SpeechMatics = {
-	submit() {
+  submit(license, lang) {
+    const command = `docker run -i
+	  -v ~/Downloads/output-audio.aac:/input.audio
+	  -e LICENSE_KEY=${license} speechmatics-docker-prod-videomenthe.jfrog.io/transcriber-${lang}:6.0.2`;
 
-	},
-	monitor() {
+    const process = cp.exec(
+      command,
+      { maxBuffer: Infinity },
+      (error, stdout, stderr) => {
+        if (error) {
+			console.error(error);
+		} else if (stderr) {
+			console.error(stderr)
+		} else {
+			console.log(stdout);
+		}
 
-	}
-}
+      }
+    );
+  },
+  monitor() {}
+};
 
 module.exports = SpeechMatics;
