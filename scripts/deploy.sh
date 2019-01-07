8e5edc3ce39c3c9c62c90469433e2237ca33a219
rolljee <n.riquelmebareiro@gmail.com>
2019-01-07 17:00:15 +0100

    Update scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,10 +14,13 @@ eval $(ssh-agent)
 ssh-add /etc/ssh/${key}
 
 # Install docker & base pkg
+echo "Install base"
 ssh ${username}@${url} 'bash -s' < ./install_base.sh
 
 # Deploy app to server
+echo "Deploy app to server"
 pm2 deploy ecosystem.config.js production setup
 
 # Make app restart on reboot
+echo "Launch reboot script"
 ssh ${username}@${url} 'bash -s' < ./startup.sh appName
--- scripts/install_base.sh
+++ scripts/install_base.sh
@@ -6,6 +6,7 @@ hasDocker=$(docker version | grep "version")
 set -e
 
 if [ ! "$hasDocker" ]; then
+	echo "Installing docker"
 	# Remove the lock
 	set +e
 	sudo rm /var/lib/dpkg/lock > /dev/null
@@ -21,14 +22,18 @@ if [ ! "$hasDocker" ]; then
 	wget -qO- https://get.docker.com/ | sudo sh
 	sudo usermod -a -G docker ${USER}
 	sudo service docker start || sudo service docker restart
+	echo "docker installed on remote host"
 fi
 
 # Install base pkg
+echo "Installing base packages"
 sudo apt-get install -y nodejs npm
 
 # Install pm2 on machine
+echo "Installing pm2"
 npm install -g pm2
 
 # Log to docker and pull the image.
+echo "Pulling speechmatics docker"
 docker login --username=videomenthe --password=AKCp5btyfoWgqqeSrjSQ1FSFDkKWDo8oSi3VjurzK2oYW5PCr9dg37J8u2G4xYPYSeGGb5JgE https://speechmatics-docker-prod-videomenthe.jfrog.io
 docker pull speechmatics-docker-prod-videomenthe.jfrog.io/transcriber-en:6.0.2
