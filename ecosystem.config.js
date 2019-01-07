99969267f6302ddd650b6a5cdeb840caae660510
rolljee <n.riquelmebareiro@gmail.com>
2019-01-07 11:29:49 +0100

    Working on remote deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: "speechmatics.js",
+      script: "index.js"
+    }
+  ],
+  deploy: {
+    // "production" is the environment name
+    production: {
+      key: "/etc/ssh/eolementhe.top",
+      user: "eolementhe",
+      host: ["13.80.28.40"],
+      ssh_options: "StrictHostKeyChecking=no",
+      ref: "origin/master",
+      repo: "https://github.com/eolementhe/speechmatics-api",
+      path: "~/speechmatics-api",
+      "post-deploy": "npm install"
+    }
+  }
+};
+
+// Visit https://pm2.io/doc/en/runtime/guide/easy-deploy-with-ssh/ for mon info
