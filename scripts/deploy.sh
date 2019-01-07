ee5d2ae80e16cfa93435ab9dd8d7c339fe1b152f
rolljee <n.riquelmebareiro@gmail.com>
2019-01-07 17:10:10 +0100

    Fix scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,12 +15,15 @@ ssh-add /etc/ssh/${key}
 
 # Install docker & base pkg
 echo "Install base"
-ssh ${username}@${url} 'bash -s' < ./install_base.sh
+echo $pwd
+ssh ${username}@${url} 'bash -s' < ./scripts/install_base.sh
 
 # Deploy app to server
+set +e
 echo "Deploy app to server"
 pm2 deploy ecosystem.config.js production setup
+set -e
 
 # Make app restart on reboot
 echo "Launch reboot script"
-ssh ${username}@${url} 'bash -s' < ./startup.sh appName
+ssh ${username}@${url} 'bash -s' < ./scripts/startup.sh appName
--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,7 +2,6 @@
 
 # This should work for ubuntu/debian
 sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp /home/$USER
-sudo env PATH=$PATH:/usr/bin /usr/local/lib/node_modules/pm2/bin/pm2 unstartup systemd -u $USER --hp /home/$USER
 
 # Save the config
 pm2 save
