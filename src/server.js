6d9269d8961ebc1297c61779cd23e950bc1e0749
rolljee <n.riquelmebareiro@gmail.com>
2019-01-09 12:49:12 +0100

    Removed bodyparser as a deps

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import routes from '../routes/routes.js';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 routes(app);
 
